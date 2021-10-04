5f650fe4225e35023437aabeca8c47206d1ad8c1
Wentao He <wentaohe@users.noreply.github.com>
2021-10-03 22:39:26 -0400

    Update utils.py

--- Codes/utils.py
+++ Codes/utils.py
@@ -2,12 +2,17 @@ import requests
 import os
 import json
 
+def feed(start_date, end_date):
+  key = os.getenv('KEY')
+  url = f'https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={key}'
+  return requests.get(url).json()
+
 def browse(page_num = 0):
-  key = os.getenv('NASA_KEY')
+  key = os.getenv('KEY')
   url = f'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={key}&page={page_num}'
   return requests.get(url).json()
 
-def feed(start_date, end_date):
-  key = os.getenv('NASA_KEY')
-  url = f'https://api.nasa.gov/neo/rest/v1/feed?api_key={key}&start_date={start_date}&end_date={end_date}'
+def lookup(astroid_id):
+  key = os.getenv('KEY')
+  url = f'https://api.nasa.gov/neo/rest/v1/neo/{astroid_id}?api_key={key}'
   return requests.get(url).json()
