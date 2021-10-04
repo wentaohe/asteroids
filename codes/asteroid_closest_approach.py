65fb7e96009610233dea97750f6690d4ec284043
Wentao He <wentaohe@users.noreply.github.com>
2021-10-04 00:05:32 -0400

    Update asteroid_closest_approach.py

--- codes/asteroid_closest_approach.py
+++ codes/asteroid_closest_approach.py
@@ -3,6 +3,17 @@ import math
 from .utils import feed, browse, lookup
 
 def asteroid_closest_approach(page_limit = None):
-    asteroids = []
-    browse_data = browse()
-    
+    closest_asteroids = []
+    browse_data = browse() 
+    number_of_pages = browse_data['page']['total_pages']
+    for page in range(0, number_of_pages):
+        asteroids = browse(page)
+        results = find(asteroids, closest_asteroids)
+    return json.dumps(results)
+
+def find(asteroids, closest_asteroids):
+    for asteroid in asteroids['near_earth_objects']:
+        if asteroid['close_approach_data']:
+            asteroid['close_approach_data'] = min(asteroid['close_approach_data'], key=lambda x:x['miss_distance']['astronomical'])
+            closest_asteroids.append(asteroid)
+    return closest_asteroids
